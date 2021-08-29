93b6bf736a0bf334b4bc6451657fd12c7e5178e7
EricCraigen <eric.craigen.cd@gmail.com>
2021-08-28 23:06:17 -0700

    WIP Transfer to PC

--- puravidasoberliving.com/resources/views/layouts/navigation.blade.php
+++ puravidasoberliving.com/resources/views/layouts/navigation.blade.php
@@ -8,9 +8,8 @@
         }
         console.log(scrollTop)
       } --}}
-    }"
-    x-on:scroll.window="minifyNavOnScroll(window.scrollY)"
-            class="bg-white border-b border-gray-100 navTopPadding sticky top-0">
+    }" @scroll.window="minifyNavOnScroll(window.scrollY)"
+    class="contianer bg-white border-b border-gray-100 navTopPadding sticky top-0 z-50">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -32,28 +31,36 @@
                     <x-nav-link :href="route('mission')" :active="request()->routeIs('mission')">
                         {{ __('Our Mission') }}
                     </x-nav-link>
-                    <div class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent ml-6 {{ Route::currentRouteName() == 'a-la-carte-recovery' ? 'border-b-2 border-indigo-400' : 'hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out' }}">
+                    <div
+                        class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent ml-6 {{ Route::currentRouteName() == 'a-la-carte-recovery' ? 'border-b-2 border-indigo-400' : 'hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out' }}">
                         <x-dropdown align="right" width="auto">
                             <x-slot name="trigger">
-                                <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                <button
+                                    class="flex items-center text-md font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                                     <div>{{ __('Á La Carte Recovery') }}</div>
 
                                     <div class="ml-1">
-                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clip-rule="evenodd" />
                                         </svg>
                                     </div>
                                 </button>
                             </x-slot>
 
                             <x-slot name="content">
-                                <x-dropdown-link :href="route('a-la-carte-recovery')" :active="request()->routeIs('a-la-carte-recovery')">
+                                <x-dropdown-link :href="route('a-la-carte-recovery')"
+                                    :active="request()->routeIs('a-la-carte-recovery')">
                                     {{ __('About PVSL') }}
                                 </x-dropdown-link>
-                                <x-dropdown-link :href="route('a-la-carte-recovery')" :active="request()->routeIs('a-la-carte-recovery')">
+                                <x-dropdown-link :href="route('a-la-carte-recovery')"
+                                    :active="request()->routeIs('a-la-carte-recovery')">
                                     {{ __('Living Guidelines') }}
                                 </x-dropdown-link>
-                                <x-dropdown-link :href="route('a-la-carte-recovery')" :active="request()->routeIs('a-la-carte-recovery')">
+                                <x-dropdown-link :href="route('a-la-carte-recovery')"
+                                    :active="request()->routeIs('a-la-carte-recovery')">
                                     {{ __('FAQ\'s') }}
                                 </x-dropdown-link>
                             </x-slot>
@@ -80,48 +87,55 @@
                 <span x-text="count"></span>
             </div> --}}
 
-                <!-- Settings Dropdown -->
-                <div class="hidden md:flex md:items-center md:ml-6">
-                    @if (Auth::check())
-                        <x-dropdown align="right" width="48">
-                            <x-slot name="trigger">
-                                <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                                    <div>{{ Auth::user()->name }}</div>
+            <!-- Settings Dropdown -->
+            <div class="hidden md:flex md:items-center md:ml-6">
+                @if (Auth::check())
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button
+                                class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                <div>{{ Auth::user()->name }}</div>
 
-                                    <div class="ml-1">
-                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                        </svg>
-                                    </div>
-                                </button>
-                            </x-slot>
+                                <div class="ml-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd"
+                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
 
-                            <x-slot name="content">
-                                <x-dropdown-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                                    {{ __('Dashboard') }}
-                                </x-dropdown-link>
-                                <!-- Authentication -->
-                                <form method="POST" action="{{ route('logout') }}">
-                                    @csrf
-                                    <x-dropdown-link :href="route('logout')"
-                                            onclick="event.preventDefault();
+                        <x-slot name="content">
+                            <x-dropdown-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                                {{ __('Dashboard') }}
+                            </x-dropdown-link>
+                            <!-- Authentication -->
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
                                                         this.closest('form').submit();">
-                                        {{ __('Log Out') }}
-                                    </x-dropdown-link>
-                                </form>
-                            </x-slot>
-                        </x-dropdown>
-                    @endif
-                </div>
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form>
+                        </x-slot>
+                    </x-dropdown>
+                @endif
+            </div>
 
 
 
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center md:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                <button @click="open = ! open"
+                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
+                            stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
@@ -139,29 +153,37 @@
             <x-responsive-nav-link :href="route('mission')" :active="request()->routeIs('mission')">
                 {{ __('Our Mission') }}
             </x-responsive-nav-link>
-            <div class="block pl-3 pr-4 border-l-4 border-transparent text-base font-medium {{ Route::currentRouteName() == 'a-la-carte-recovery' ? 'border-indigo-400' : 'text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out' }}">
+            <div
+                class="block pl-3 pr-4 border-l-4 border-transparent text-base font-medium {{ Route::currentRouteName() == 'a-la-carte-recovery' ? 'border-indigo-400' : 'text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out' }}">
                 {{--  --}}
                 <x-dropdown align="left" width="auto">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center w-auto pr-4 py-2 text-base font-medium text-gray-500 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                        <button
+                            class="inline-flex items-center w-auto pr-4 py-2 text-base font-medium text-gray-500 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                             <div>{{ __('Á La Carte Recovery') }}</div>
 
                             <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd"
+                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                        clip-rule="evenodd" />
                                 </svg>
                             </div>
                         </button>
                     </x-slot>
 
                     <x-slot name="content">
-                        <x-dropdown-link :href="route('a-la-carte-recovery')" :active="request()->routeIs('a-la-carte-recovery')">
+                        <x-dropdown-link :href="route('a-la-carte-recovery')"
+                            :active="request()->routeIs('a-la-carte-recovery')">
                             {{ __('About PVSL') }}
                         </x-dropdown-link>
-                        <x-dropdown-link :href="route('a-la-carte-recovery')" :active="request()->routeIs('a-la-carte-recovery')">
+                        <x-dropdown-link :href="route('a-la-carte-recovery')"
+                            :active="request()->routeIs('a-la-carte-recovery')">
                             {{ __('Living Guidelines') }}
                         </x-dropdown-link>
-                        <x-dropdown-link :href="route('a-la-carte-recovery')" :active="request()->routeIs('a-la-carte-recovery')">
+                        <x-dropdown-link :href="route('a-la-carte-recovery')"
+                            :active="request()->routeIs('a-la-carte-recovery')">
                             {{ __('FAQ\'s') }}
                         </x-dropdown-link>
                     </x-slot>
@@ -197,8 +219,7 @@
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
 
-                        <x-responsive-nav-link :href="route('logout')"
-                                onclick="event.preventDefault();
+                        <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
                                             this.closest('form').submit();">
                             {{ __('Log Out') }}
                         </x-responsive-nav-link>
@@ -216,12 +237,14 @@
     function minifyNavOnScroll(scrollPos) {
         if (scrollPos == 0) {
             $('nav').addClass('navTopPadding').removeClass('navTopPaddingRemove');
+            $('.nav-scroll-toggle').addClass('navTopSize').removeClass('navTopSizeReduce');
             // return atTopOfPage = true;
             // console.log(atTopOfPage);
             // console.log(scrollPos);
         } else {
             // return atTopOfPage = false;
             $('nav').removeClass('navTopPadding').addClass('navTopPaddingRemove');
+            $('.nav-scroll-toggle').addClass('navTopSizeReduce').removeClass('navTopSize');
             // console.log(atTopOfPage);
             // console.log(scrollPos);
         }
@@ -230,4 +253,5 @@
         //     $('nav').addClass('navTopPadding').removeClass('sticky').removeClass('top-0');
         // }
     }
+
 </script>
--- puravidasoberliving.com/tailwind.config.js
+++ puravidasoberliving.com/tailwind.config.js
@@ -1,32 +1,65 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
     // prefix: 'tw-',
     purge: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         screens: {
-            sm: '480px',
-            md: '768px',
-            lg: '976px',
-            xl: '1440px',
+            sm: "480px",
+            md: "768px",
+            lg: "976px",
+            xl: "1440px",
         },
+        // colors: {
+        //     transparent: "transparent",
+        //     current: "currentColor",
+        //     blue: {
+        //         light: "#85d7ff",
+        //         DEFAULT: "#1fb6ff",
+        //         dark: "#009eeb",
+        //     },
+        //     pink: {
+        //         light: "#ff7ce5",
+        //         DEFAULT: "#ff49db",
+        //         dark: "#ff16d1",
+        //     },
+        //     gray: {
+        //         darkest: "#17e34d",
+        //         dark: "#17e34d",
+        //         DEFAULT: "#17e34d",
+        //         light: "#17e34d",
+        //         lightest: "#17e34d",
+        //     },
+        // ...this.theme.colorsc,
+        // },
         extend: {
+            colors: {
+                transparent: "transparent",
+                current: "currentColor",
+                black: colors.black,
+                white: colors.white,
+                gray: colors.trueGray,
+                indigo: colors.indigo,
+                red: colors.rose,
+                yellow: colors.amber,
+            },
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
             },
         },
     },
 
     variants: {
         extend: {
-            opacity: ['disabled'],
+            opacity: ["disabled"],
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [require("@tailwindcss/forms")],
 };
