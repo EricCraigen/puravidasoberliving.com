274ba589adffdfc21ec93c7462aa1b62551dbeb6
EricCraigen <eric.craigen.cd@gmail.com>
2021-09-03 11:18:12 -0700

    Fleshing out the remainder of the login system pages. WIP

--- puravidasoberliving.com/resources/views/auth/verify-email.blade.php
+++ puravidasoberliving.com/resources/views/auth/verify-email.blade.php
@@ -37,3 +37,66 @@
         </div>
     </x-auth-card>
 </x-guest-layout>
+
+<x-app-layout>
+
+    <div class="min-h-screen bg-gray-50 flex flex-col items-center justify-center py-12 sm:px-6 lg:px-8">
+
+        <div class="sm:mx-auto sm:w-full sm:max-w-md">
+            <a class="inline-flex w-full justify-center" href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+            <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900" id="login-register-notification">
+                {{ __('Resend Verification Email') }}
+            </h2>
+        </div>
+
+        <div class="w-3/12 bg-white py-8 px-4 mt-8 shadow sm:rounded-lg sm:px-10">
+
+            <div class="mb-4 text-sm text-gray-600">
+                {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+            </div>
+
+            <div class="flex mb-4 text-sm text-green-500">
+                <span>{{ session('status') }}</span>
+                {{-- <div class="flex items-center justify-center rounded-md border p-2 cursor-pointer font-black text-black">X</div> --}}
+            </div>
+
+            <div class="mt-4 flex items-center justify-between">
+
+
+                <div>
+                    <form method="POST" action="{{ route('verification.send') }}">
+                        @csrf
+                        <button type="submit"
+                        class="w-3/6 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500""
+                        onclick="e.preventDefault();
+                            this.closest('form').submit();
+                        ">
+                            {{ __('Resend Verification Email') }}
+                        </button>
+                    </form>
+                </div>
+
+                <div>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <x-button>
+                            {{ __('Logout') }}
+                        </x-button>
+                    </form>
+                </div>
+
+{{--
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
+                        {{ __('Log Out') }}
+                    </button>
+                </form> --}}
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
